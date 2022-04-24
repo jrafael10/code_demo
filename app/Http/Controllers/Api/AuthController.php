e6126da5ca6498a4bcbf070c4447afb21bdc7c85
jrafael10 <jesserafael102193@gmail.com>
2022-04-24 16:51:42 -0400

    Setting up the registration process and the models

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        // Ensures that the data being submitted to register is valid
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        // If everything is valid, create a new user
+        // and return a successful 204 response meaning an entity has been created.
+            if(!$validator->fails()) {
+                $user = User::create([
+                   'name' =>  $request->get('name'),
+                    'email' => $request->get('email'),
+                    'password' => bcrypt($request->get('password'))
+                ]);
+
+                return response()->json(['message' => "Succesful registration" ], 204);
+            }
+        // Return the errors from the validator and a failure flag.
+        return response()->json([
+            'success' => false,
+            'errors' => $validator->errors()
+        ], 422);
+
+    }
+
+    /*
+     * Authenticates a user in the system
+     *
+     */
+
+    public function login()
+    {
+
+    }
+}
